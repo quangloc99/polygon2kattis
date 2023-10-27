6fb9337672a2a045dfb7ae51cd76947653bdf698
Tran Quang Loc <quangloctran99@gmail.com>
2023-10-27 22:03:02 +0700

    Fix typing. Add --test-generation-info

--- polygon2kattis.py
+++ polygon2kattis.py
@@ -1,9 +1,11 @@
 import argparse
-from pathlib import Path
-from zipfile import ZipFile
+import shutil
+import textwrap
 import xml.etree.ElementTree as ET
 from dataclasses import dataclass
-import shutil
+from pathlib import Path
+from zipfile import ZipFile
+
 
 @dataclass
 class SupportedLanguage:
@@ -54,6 +56,16 @@ def build_argparser():
                            help='Problem license',
                            default='cc by-sa'
                            )
+    argparser.add_argument('--test-generation-info',
+                           action='store_true',
+                           help=textwrap.dedent("""
+                           Generate information about test generation, for the purpose of reviewing.
+                           - The generators will be copied.
+                           - The test generation script will be generated.
+                           All the generated and copied file will be in the data folder.
+                           """),
+                           default=False
+                           )
     return argparser
 
 
@@ -63,13 +75,16 @@ class Polygon2Kattis:
                  out_dir: Path,
                  lang: SupportedLanguage,
                  verbose: bool,
+                 test_generation_info: bool,
                  license: str
                  ):
         self.package_zip_file = package_zip_file
         self.package = ZipFile(package_zip_file, 'r')
         self.lang = lang
         self.verbose = verbose
-        self.problem_data = ET.fromstringlist(self.package.open('problem.xml'))
+        self.test_generation_info = test_generation_info
+
+        self.problem_data = ET.fromstringlist(self.package.read('problem.xml').decode())
         self.testlib_path = Path(__file__).parent / 'testlib.h'
         self.license = license
         
@@ -79,14 +94,16 @@ class Polygon2Kattis:
         self.data_path = self.add_folder(self.out_dir, 'data')
         self.sample_data_path = self.add_folder(self.data_path, 'sample')
         self.secret_data_path = self.add_folder(self.data_path, 'secret')
+
+        self.generator_names: set[str] = set()
         
         self.check_type = ''
         
-    def force_mkdir(self, path):
+    def force_mkdir(self, path: Path):
         path.mkdir(parents=True, exist_ok=True)
         return path
         
-    def add_folder(self, path, *subfolders):
+    def add_folder(self, path: Path, *subfolders) -> Path:
         for subfolder in subfolders:
             path = self.force_mkdir(path / subfolder)
         return path
@@ -95,7 +112,7 @@ class Polygon2Kattis:
         if self.verbose:
             print(*args)
         
-    def extract_package_member_to(self, member, dest):
+    def extract_package_member_to(self, member: str, dest: Path):
         self.log('extracting', member, dest)
         with self.package.open(member, 'r') as source, open(dest, 'wb') as target:
             shutil.copyfileobj(source, target)
@@ -158,18 +175,46 @@ class Polygon2Kattis:
         testsets = self.problem_data.findall('./judging/testset')
         for testset in testsets:
             self.process_testset(testset)
-        
-    def process_testset(self, testset):
-        testset_name = testset.get('name')
+
+        if self.test_generation_info:
+            files_to_exclude = set([
+                'files/olymp.sty',
+                'files/problem.tex',
+                'files/statements.ftl',
+            ])
+            for resource_tags in self.problem_data.findall('./files/resources/file'):
+                file_path = resource_tags.get('path', '')
+                if file_path in files_to_exclude:
+                    continue
+                self.extract_package_member_to(file_path, self.secret_data_path / Path(file_path).name)
+
+            for executable_tags in self.problem_data.findall('./files/executables/executable/source'):
+                file_path = executable_tags.get('path', '')
+                if any(generator_name in file_path for generator_name in self.generator_names):
+                    self.extract_package_member_to(file_path, self.secret_data_path / Path(file_path).name)
+
+    def process_testset(self, testset: ET.Element):
+        testset_name = testset.get('name', '')
         self.log('Processing testset', testset_name)
         
         cur_sample_data_path = self.sample_data_path
         cur_secret_data_path = self.add_folder(self.secret_data_path, testset_name)
+
+        input_path_pattern_tag = testset.find('input-path-pattern')
+        answer_path_pattern_tag = testset.find('answer-path-pattern')
+        testcount_tag = testset.find('test-count')
+        if (input_path_pattern_tag is None or
+            answer_path_pattern_tag is None or
+            testcount_tag is None):
+            self.log(f'Skip processing testset {testset_name}. Tags not found.')
+            return  
         
-        input_path_pattern = testset.find('input-path-pattern').text
-        answer_path_pattern = testset.find('answer-path-pattern').text
-        testcount = int(testset.find('test-count').text)
+        input_path_pattern = input_path_pattern_tag.text or ''
+        answer_path_pattern = answer_path_pattern_tag.text or ''
+        testcount = int(testcount_tag.text or '0')
         test_tags = testset.findall('./tests/test')
+
+        generation_scripts: list[str] = []
         
         for test_id in range(1, testcount + 1):
             input_filename = input_path_pattern % test_id
@@ -178,16 +223,35 @@ class Polygon2Kattis:
             is_sample = test_tag.get('sample') == 'true'
             
             dest_path = cur_sample_data_path if is_sample else cur_secret_data_path
-            self.extract_package_member_to(input_filename, dest_path / f'{test_id}.in')
-            self.extract_package_member_to(answer_filename, dest_path / f'{test_id}.ans')
+            input_file_name = f'{test_id}.in'
+            output_file_name = f'{test_id}.ans'
+            self.extract_package_member_to(input_filename, dest_path / input_file_name)
+            self.extract_package_member_to(answer_filename, dest_path / output_file_name)
+            
+            test_generation_script = test_tag.get('cmd')
+            if test_generation_script is not None:
+                generation_scripts.append(f'{test_generation_script} > {input_file_name}')
+                self.generator_names.add(test_generation_script.split()[0])
+        if self.test_generation_info:
+            path = cur_secret_data_path/ '_gen-test-script'
+            disclaimer = textwrap.dedent("""
+            # Actual tests were generated on Polygon.
+            # This file is generated and not meant to be ran.
+            # This file is here to demonstrate how the generators were used to generate the test.
+            """)
+            path.write_text(disclaimer + '\n' + '\n'.join(generation_scripts))
+            self.log(f'Write f{path}')
             
     def process_solutions(self):
         self.log('Processing solutions')
         solution_tags = self.problem_data.findall('./assets/solutions/solution')
         submission_path = self.out_dir / 'submissions'
         for solution_tag in solution_tags:
+            source_tag = solution_tag.find('source')
             tag = solution_tag.get('tag')
-            path = solution_tag.find('source').get('path')
+            if source_tag is None:
+                continue
+            path = source_tag.get('path', '')
             if tag in ['accepted', 'main']:
                 sol_out_dir = self.add_folder(submission_path, 'accepted')
             elif tag == 'time-limit-exceeded':
@@ -215,10 +279,12 @@ class Polygon2Kattis:
         else:
             self.checker_type = 'custom'
         source_tag = checker_tag.find('source')
-        source_path = source_tag.get('path')
+        if source_tag == None:
+            return
+        source_path = source_tag.get('path', '')
         checker_out_dir = self.add_folder(self.out_dir, 'output_validators', 'checker')
         self.extract_package_member_to(source_path, checker_out_dir / Path(source_path).name)
-        if 'cpp' in source_tag.get('type'):
+        if 'cpp' in source_tag.get('type', ''):
             shutil.copy(self.testlib_path, checker_out_dir)
     
     def _process_validator(self):
@@ -227,10 +293,12 @@ class Polygon2Kattis:
             return
         validator_tag = validator_tags[0]
         source_tag = validator_tag.find('source')
-        source_path = source_tag.get('path')
+        if source_tag is None:
+            return
+        source_path = source_tag.get('path', '')
         validator_out_dir = self.add_folder(self.out_dir, 'input_validators', 'extracted_validator')
         self.extract_package_member_to(source_path, validator_out_dir / Path(source_path).name)
-        if 'cpp' in source_tag.get('type'):
+        if 'cpp' in source_tag.get('type', ''):
             shutil.copy(self.testlib_path, validator_out_dir)
             
     def _process_interactor(self):
@@ -264,7 +332,8 @@ def main():
             out_dir=args.out_dir,
             lang=args.lang,
             verbose=args.verbose,
-            license=args.license
+            license=args.license,
+            test_generation_info=args.test_generation_info
             )
     p2k.process_statement()
     p2k.process_tests()
